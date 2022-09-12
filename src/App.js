d1722555a65e7c7e907fa36e5527b3d782779fc4
srinaththota <srinath.thota@gmail.com>
2022-09-12 14:56:55 +0200

    home page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,32 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <Login/>
+      <h1>Home Page</h1>
+      <div className="components">
+        <button>Guide setup</button>
+        <button>Templates</button>
+        <button>Simulations</button>
+        <button>published workflow</button>
+        <button>config</button>
+      </div>
+      <div className='subcomponents'>
+        <div className='childcomponents'>
+          <button>Guide setup</button>
+          <div>1.</div>
+          <div>2.</div>
+          <div>3.</div>
+          <div>4.</div>
+          <div>5.</div>
+          </div>
+        <div className='childcomponents'>
+          <button>template setup</button>
+          <div>1.</div>
+          <div>2.</div>
+          <div>3.</div>
+          <div>4.</div>
+          <div>5.</div>
+          </div>
+      </div>
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { LoginAction } from "../store/actions/LoginAction";
+import classes from "./Login.module.css"
+import { FormControlLabel } from '@mui/material';
+import { Paper,Typography, Checkbox } from '@mui/material';
+
 const Login = () =>{
    
     const [username,setUsername] = useState("")
@@ -20,10 +24,12 @@ const Login = () =>{
     }
     return(
         <>
-        <input type="text" onChange={handleUsername} value={username}/><br/>
+        <input className={classes.input} type="text" onChange={handleUsername} value={username}/><br/>
         <input type="password" onChange={handlePassword} value={password}/><br/>
         <button type="submit" onClick={submitHandler}>Submit</button>
-        
+        <Paper style={{display:"flex", justifyContent:"center", alignItems:"center", marginTop:80, marginLeft:70, marginRight:70}}>
+        <Typography variant="title"  >Configure Component</Typography>
+          </Paper>
         </>
     )
 }
