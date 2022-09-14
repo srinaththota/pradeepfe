208973f0ccd58e93d11c74adb384dcf96ce1d1c7
srinaththota <srinath.thota@gmail.com>
2022-09-14 13:50:04 +0200

    added routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import Login from './components/Login';
+import GuideSetup from './pages/GuideSetup';
+import HConfig from './pages/HConfig';
+import PublishedWorkFlow from './pages/PublishedWorkFlow';
+import Simulations from './pages/Simulations';
+import Templates from './pages/Templates';
+import { Route, Link, Routes } from 'react-router-dom'
+
 import './App.css';
 
 function App() {
@@ -6,15 +13,15 @@ function App() {
     <div className='App'>
       <h1>Home Page</h1>
       <div className="components">
-        <button>Guide setup</button>
-        <button>Templates</button>
-        <button>Simulations</button>
-        <button>published workflow</button>
-        <button>config</button>
+        <Link className='childelements' to="/guildsetup" >GuideSetup</Link>
+        <Link className='childelements' to="/templates">Templates</Link>
+        <Link className='childelements' to="/simulations">Simulations</Link>
+        <Link className='childelements' to="/publishwf">PublishedWorkFlow</Link>
+        <Link className='childelements' to="hconfig">HConfig</Link>
       </div>
       <div className='subcomponents'>
         <div className='childcomponents'>
-          <button>Guide setup</button>
+          <div>GuideSetup</div>
           <div>1.</div>
           <div>2.</div>
           <div>3.</div>
@@ -22,7 +29,7 @@ function App() {
           <div>5.</div>
           </div>
         <div className='childcomponents'>
-          <button>template setup</button>
+          <div>template setup</div>
           <div>1.</div>
           <div>2.</div>
           <div>3.</div>
@@ -30,6 +37,17 @@ function App() {
           <div>5.</div>
           </div>
       </div>
+      <main>
+        <Routes>
+          <>
+          <Route path="guildsetup" element={<GuideSetup/>}/>
+          <Route path="templates" element={<Templates/>} />
+          <Route path="simulations" element={<Simulations/>}/>
+          <Route path="publishwf" element={<PublishedWorkFlow/>}></Route>
+          <Route path="hconfig" element={<HConfig/>}></Route>
+          </>
+        </Routes>
+      </main>
     </div>
   );
 }
