f05f33d48fabc13b899f6eab70c2b2f3a5cd5dfc
srinaththota <srinath.thota@gmail.com>
2022-09-19 20:55:45 +0200

    added func

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,8 +2,7 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { LoginAction } from "../store/actions/LoginAction";
 import classes from "./Login.module.css"
-import { FormControlLabel } from '@mui/material';
-import { Paper,Typography, Checkbox } from '@mui/material';
+
 
 const Login = () =>{
    
@@ -17,20 +16,19 @@ const Login = () =>{
     const handlePassword=(e)=>{
         setPassword(e.target.value)
     }
-    const submitHandler=()=>{
+    const submitHandler=(e)=>{
+        e.preventDefault();
         console.log("reached here")
         loginDispatch(LoginAction(username,password))
  
     }
     return(
-        <>
+        <form onSubmit={submitHandler}>
         <input className={classes.input} type="text" onChange={handleUsername} value={username}/><br/>
         <input type="password" onChange={handlePassword} value={password}/><br/>
-        <button type="submit" onClick={submitHandler}>Submit</button>
-        <Paper style={{display:"flex", justifyContent:"center", alignItems:"center", marginTop:80, marginLeft:70, marginRight:70}}>
-        <Typography variant="title"  >Configure Component</Typography>
-          </Paper>
-        </>
+        <button type="submit">Submit</button>
+        
+        </form>
     )
 }
 
--- src/store/actions/LoginAction.js
+++ src/store/actions/LoginAction.js
@@ -3,13 +3,24 @@ import * as actionTypes from "./ActionTypes"
 export const LoginAction=(username,password)=>{
     console.log(username,password)
         return async dispatch=>{
-    
+            const hash='Auth'
+            const headers = {
+                'Content-Type': 'text/pain',
+                Accept: 'application/json',
+                //"Authorization": "Basic dXNlcjowM2VhN2JhYS1mMTQ0LTQ5YWMtOGFhMy02NDE4YWJiNzdhMTk=",
+                Authorization: `Basic ${hash}`,
+                method:'POST',
+                credentials: 'include',
+            };
+            
+            const config = {
+                headers
+            };
+            const data=JSON.stringify({ username: username,password: password })
+            
             try{
-             const responseData=axios.post("http://localhost:8080/login",{
-                        username:username,
-                        password:password
-                    }
-                ).then(()=>{
+             const responseData=axios.post("http://localhost:8080/authenticate",
+             data,config).then(()=>{
                     console.log(responseData)
                     dispatch({
                         type:actionTypes.LOGIN,
